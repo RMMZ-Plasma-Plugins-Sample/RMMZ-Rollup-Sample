8b16339179e4745e1d4b699995e209f50f1c6398
elleonard <6374339+elleonard@users.noreply.github.com>
2023-04-23 15:06:13 +0900

    2b509bcf0c2a97b4589e22370eb51b13389aadd5

--- Plasma_ThinMessageWindow.js
+++ Plasma_ThinMessageWindow.js
@@ -1,9 +1,10 @@
-// Plasma_ThinMessageWindow 1.0.0
+// Plasma_ThinMessageWindow 1.1.0
 // Copyright (c) 2023 Plasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2023/04/23 1.1.0 スイッチと幅をパラメータ化する
  * 2023/04/16 1.0.0 公開
  */
 
@@ -15,23 +16,46 @@
  * @target MZ
  * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
  *
+ * @param thinMessageWindowSwitch
+ * @desc このスイッチがONのとき、メッセージウィンドウの幅を細くします。
+ * @text スイッチ
+ * @type switch
+ * @default 0
+ *
+ * @param thinMessageWindowWidth
+ * @desc 細くなったメッセージウィンドウの幅を指定します。
+ * @text 幅
+ * @type number
+ * @default 350
+ *
  * @help
- * version: 1.0.0
+ * version: 1.1.0
  * 指定したスイッチがONの時、メッセージウィンドウの幅を細くします。
  */
 
 (() => {
   'use strict';
 
+  const pluginName = document.currentScript.src.replace(/^.*\/(.*).js$/, function () {
+    return arguments[1];
+  });
+
+  const pluginParameters = PluginManager.parameters(pluginName);
+
+  const settings = {
+    thinMessageWindowSwitch: Number(pluginParameters.thinMessageWindowSwitch || 0),
+    thinMessageWindowWidth: Number(pluginParameters.thinMessageWindowWidth || 350),
+  };
+
   /**
    * メッセージウィンドウの幅を細くするスイッチ
    */
-  const THIN_MESSAGE_WINDOW_SWITCH = 12;
+  const THIN_MESSAGE_WINDOW_SWITCH = settings.thinMessageWindowSwitch;
 
   /**
    * 細くなったメッセージウィンドウの幅
    */
-  const THIN_MESSAGE_WINDOW_WIDTH = 350;
+  const THIN_MESSAGE_WINDOW_WIDTH = settings.thinMessageWindowWidth;
 
   function Window_Message_ThinMessageWindowMixIn(windowClass) {
     const _initialize = windowClass.initialize;
